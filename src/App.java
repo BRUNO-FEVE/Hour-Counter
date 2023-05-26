b86e6e7342028cf289506077739f102b37b30464
Bruno Fevereiro <br.fevereiro@terra.com.br>
2023-05-26 12:12:25 -0300

    User Menu Done

--- src/App.java
+++ src/App.java
@@ -1,9 +1,11 @@
 import pages.LoginPage;
 import pages.RegisterPage;
+import pages.UserMenuPage;
 
 public class App {
     public static void main(String[] args){
         // new RegisterPage();
-        new LoginPage();
+        //new LoginPage();
+        new UserMenuPage();
     }
 }
