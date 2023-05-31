0182e8c4081a0d69bd177d6f0c26b5f077cc7b10
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2023-05-31 15:42:22 -0300

    Subtitles for TotalHour

--- src/pages/HourViewPage.java
+++ src/pages/HourViewPage.java
@@ -3,7 +3,11 @@ import components.HourViewTableModel;
 
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
 
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
@@ -11,11 +15,11 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
-import javax.swing.table.DefaultTableCellRenderer;
+
 
 public class HourViewPage extends JFrame{
     
-    private JLabel totalHoursMessageLabel, tabelMassageLabel, totalHoursLabel;
+    private JLabel totalHoursMessageLabel, tabelMassageLabel, totalHoursLabel, redLabel, greenLabel;
 
     public HourViewPage(String[] columnName, Object[][] userData, String totalHours) {
         super("Histório de Horas");
@@ -23,6 +27,11 @@ public class HourViewPage extends JFrame{
         totalHoursMessageLabel = new JLabel("Horas Totais: ");
         tabelMassageLabel = new JLabel("Suas ultimas Tarefas:");
         totalHoursLabel = new JLabel(totalHours);
+
+        greenLabel = new JLabel("Voçê NÃO atingiu as horas neccessarias");
+        redLabel = new JLabel("Voçê atingiu as horas neccessarias");
+
+        //redSquare.drawRect(EXIT_ON_CLOSE, ABORT, WIDTH, HEIGHT);
 
         // Layout
 
@@ -50,20 +59,49 @@ public class HourViewPage extends JFrame{
         JPanel totalHourPanel = new JPanel();
         totalHourPanel.setLayout(new FlowLayout());
 
+        JPanel subtitles = new JPanel() {
+            @Override
+            public void paintComponent(Graphics graphic) {
+                super.paintComponents(graphic);
+                
+                Graphics2D square = (Graphics2D) graphic;
+
+                square.setColor(Color.decode("#31572c"));
+                square.fill(new Rectangle2D.Double(85, 8, 10, 10));
+
+                square.setColor(Color.decode("#c1121f"));
+                square.fill(new Rectangle2D.Double(70, 29, 10, 10));
+
+                square.dispose();
+                graphic.dispose();
+            }
+        };
+        subtitles.setLayout(new FlowLayout(FlowLayout.CENTER));
+        subtitles.setPreferredSize(new Dimension(0, 50));
+        subtitles.setBorder(new EmptyBorder(0, 10, 0, 0));
+
+        JPanel tableTittle = new JPanel();
+        tableTittle.setLayout(new FlowLayout(FlowLayout.LEFT));
+        
         JTable hourHistoricTable = new JTable();
         hourHistoricTable.setModel(new HourViewTableModel(columnName, userData));
-
-        
 
         totalHourPanel.add(totalHoursMessageLabel);
         totalHourPanel.add(totalHoursLabel);
 
+        subtitles.add(redLabel);
+        subtitles.add(greenLabel);
+
+        tableTittle.add(tabelMassageLabel);
+
         screanLayoutPanel.add(totalHourPanel);
-        screanLayoutPanel.add(tabelMassageLabel);
+        screanLayoutPanel.add(subtitles);
+        screanLayoutPanel.add(tableTittle);
         screanLayoutPanel.add(hourHistoricTable);
 
         caixa.add(screanLayoutPanel);
 
+        pack();
         setSize(600, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
