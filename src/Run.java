c9d562f6b6664c578f24bb4a19c28b06aa42debf
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2023-06-05 11:07:58 -0300

    Merged sql into dev

--- src/Run.java
+++ src/Run.java
@@ -1,22 +1,5 @@
-public class Test {
+public class Run {
     public static void main(String[] args){
-        //new RegisterPage();
-        //LoginPage login = new LoginPage();
-        //login.openTheLoginPage();
-        //new UserMenuPage();
-        
-        
-        //JPanel caixa = new JPanel();
-
-        //LoginPage login = new LoginPage("Login", caixa);
-        //UserMenuPage menu = new UserMenuPage("Menu", caixa);
-        //RegisterPage register = new RegisterPage("Registri-se", caixa);
-        
-        //new App(login);
-
-        //new Application(login, menu, register);
-
-        //new HourViewPage(columnNameList, userData, "40:00:00");
         new App();
     }
 }
